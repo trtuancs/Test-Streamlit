ca6e2138c77b41a67014d68f7299da12e727698f
Edgar Le <tuanlt@galaxy.com.vn>
2021-03-10 11:49:33 +0700

    Modify

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
 import streamlit as st
-import os
 import pandas as pd
+import base64
+from io import BytesIO
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
+
+
 
 st.title('Wellcome to CHECK TOOL')
 option = st.selectbox("Please select your option: ", 
@@ -13,4 +34,4 @@ if option == 'Check Transaction':
     if file_input is not None:
         st.text("We are checking month_key: {}".format(month_key))
         df2 = pd.read_excel(file_input,'MOMO',usecols="A,B,F",dtype=str).astype(str)
-        st.table(df2)
+        st.markdown(get_table_download_link(df2), unsafe_allow_html=True)
