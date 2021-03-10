05b9d2b5d8a32de844639e337c2b82135262b5a8
Edgar Le <tuanlt@galaxy.com.vn>
2021-03-10 11:45:01 +0700

    Modify

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ st.title('Wellcome to CHECK TOOL')
 option = st.selectbox("Please select your option: ", 
                     ['Check Transaction', 'Check VMG'])
 if option == 'Check Transaction':
-    st.subheader("You are using Check Transaction Function")
+    st.text("You are using Check Transaction Function")
     month_key = st.text_input("Enter month_key") 
     # st.button('Add file transaction to check')
     file_input = st.file_uploader("Upload File Transaction",type=['xlsx'])
     if file_input is not None:
-        st.write("OK--------------------------- {}".format(month_key))
+        st.text("We are checking month_key: {}".format(month_key))
         df2 = pd.read_excel(file_input,'MOMO',usecols="A,B,F",dtype=str).astype(str)
         st.table(df2)
