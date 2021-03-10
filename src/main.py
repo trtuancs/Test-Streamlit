58ea3ada271ef363219e71b4ef352bbdae44ff9f
Edgar Le <tuanlt@galaxy.com.vn>
2021-03-10 11:57:28 +0700

    Modify

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def get_table_download_link(df):
     """
     val = to_excel(df)
     b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx"> {}</a>'.format('test') # decode b'abc' => abc
 
 
 
