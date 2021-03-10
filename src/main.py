5723e972e28ef48df2d168c22ece139e62f9d90a
Edgar Le <tuanlt@galaxy.com.vn>
2021-03-10 11:25:49 +0700

    Modify

--- src/main.py
+++ src/main.py
@@ -7,9 +7,10 @@ option = st.selectbox("Please select your option: ",
                     ['Check Transaction', 'Check VMG'])
 if option == 'Check Transaction':
     st.subheader("You are using Check Transaction Function")
-    if st.button('Add file from account'):
+    if st.button('Add file transaction to check'):
         file_input = st.file_uploader("Upload File",type=['xlsx'])
-        if file_input:
-            # st.write(str(type(file_input)))
+        if file_input is not None:
+
+            st.write("OK---------------------------")
             df2 = pd.read_excel(file_input,'MOMO',usecols="A,B,F",dtype=str).astype(str)
             st.table(df2)
