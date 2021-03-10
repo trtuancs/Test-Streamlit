db4ac4247e8f8524daae9e5f8567dbae523aca76
Edgar Le <tuanlt@galaxy.com.vn>
2021-03-10 12:33:16 +0700

    Modify

--- src/main.py
+++ src/main.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 from result import *
 
- # decode b'abc' => abc
-
-
 
 st.title('Wellcome to CHECK TOOL')
 option = st.selectbox("Please select your option: ", 
@@ -12,9 +9,9 @@ option = st.selectbox("Please select your option: ",
 if option == 'Check Transaction':
     st.text("You are using Check Transaction Function")
     month_key = st.text_input("Enter month_key") 
-    # st.button('Add file transaction to check')
     file_input = st.file_uploader("Upload File Transaction",type=['xlsx'])
     if file_input is not None:
-        st.text("We are checking month_key: {}".format(month_key))
-        df2 = pd.read_excel(file_input,'MOMO',usecols="A,B,F",dtype=str).astype(str)
-        st.markdown(get_table_download_link(df2), unsafe_allow_html=True)
+        if st.button('Begin checking'):
+            st.text("We are checking month_key: {}".format(month_key))
+            df2 = pd.read_excel(file_input,'MOMO',usecols="A,B,F",dtype=str).astype(str)
+            st.markdown(get_table_download_link(df2), unsafe_allow_html=True)
