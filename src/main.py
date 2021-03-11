af2e8210b86e5829d965b0e93e6408ae507bf553
Edgar Le <tuanlt@galaxy.com.vn>
2021-03-12 01:31:25 +0700

    Fix error

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ if option == 'Check Transaction':
     st.text("You are using Check Transaction Function")
     month_key = st.text_input("Enter month_key")
     file_input = st.file_uploader("Upload File Transaction", type=['xlsx'])
-    if file_input is not None:
+    if (file_input is not None) and (month_key is not None):
         if st.button('Begin checking'):
             st.text("We are checking month_key: {}".format(month_key))
             config = Config('Staging').get_config()
